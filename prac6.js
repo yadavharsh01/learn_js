7de8862761c2ec4adc056f74fe3c12773a8f989d
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-03-06 17:19:22 +0530

    classes obj ,inheritance

---
+++ prac6.js
@@ -0,0 +1,31 @@
+//                   inhertance
+//  let data="initial web data"
+// class user{
+//     constructor(name, email){
+//         this.name=name;
+//         this.email=email;
+//     }
+//     viewData(){
+//             console.log(data)
+//     }
+// }
+// class admin extends user{
+//     constructor(name, email){
+//         super(name, email);
+//     }
+//     editData()
+//     {
+//         data="new value"
+//     }
+
+// }
+
+// let stud1=new user("123","123@123");
+// let stud2=new user("213","213@123");
+// let stud3=new user("321","321@123");
+
+// let teach1=new user("12","12@123");
+
+// let admin1=new admin("1","1@123");
+
+//              error handling
